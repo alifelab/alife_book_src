24255952a081324d91e9a40248387930a407b36d
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2017-03-31 15:22:27 +0900

    Refactering: add comment and change a bit to readability.

--- chapter02_CA/game_of_life.py
+++ chapter02_CA/game_of_life.py
@@ -10,15 +10,34 @@ ax.set_xticks([])
 ax.set_yticks([])
 ax.grid(True)
 
+field_width = 100
+field_height = 100
+
 ### random ###
-# space = np.random.randint(2, size=(100,100), dtype=np.int8)
+# space = np.random.randint(2, size=(field_height,field_width), dtype=np.int8)
 
 ### glider ###
-space = np.zeros((100,100), dtype=np.int8)
-space[2:6,2:6] = np.array([[0,0,0,0],
-                           [0,0,1,0],
-                           [0,0,0,1],
-                           [0,1,1,1]])
+# space = np.zeros((field_height,field_width), dtype=np.int8)
+# glider = np.array(
+# [[0,0,0,0],
+#  [0,0,1,0],
+#  [0,0,0,1],
+#  [0,1,1,1]])
+# space[2:2+glider.shape[0],2:2+glider.shape[1]] = glider
+
+### glider gun ###
+space = np.zeros((field_height,field_width), dtype=np.int8)
+glider_gun = np.array(
+[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+ [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+ [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+ [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+space[2:2+glider_gun.shape[0],2:2+glider_gun.shape[1]] = glider_gun
 
 img = ax.imshow(space, interpolation='nearest', cmap='Greys')
 
@@ -27,11 +46,20 @@ def update(frame):
     next_space = np.empty(space.shape, dtype=np.int8)
     for i in range(space.shape[0]):
         for j in range(space.shape[1]):
-            target_cell_state = space[i,j]
-            neighbor_cell_sum = np.sum(space[i-1:i+2, j-1:j+2]) - target_cell_state
-            if target_cell_state == 0 and neighbor_cell_sum == 3:
+            # nw: north west, ne: north east, c: center ...
+            nw = space[i-1,j-1]
+            n  = space[i-1,j]
+            ne = space[i-1,(j+1)%space.shape[1]]
+            w  = space[i,j-1]
+            c  = space[i,j]
+            e  = space[i,(j+1)%space.shape[1]]
+            sw = space[(i+1)%space.shape[0],j-1]
+            s  = space[(i+1)%space.shape[0],j]
+            se = space[(i+1)%space.shape[0],(j+1)%space.shape[1]]
+            neighbor_cell_sum = nw + n + ne + w + e + sw + s + se
+            if c == 0 and neighbor_cell_sum == 3:
                 next_space[i,j] = 1
-            elif target_cell_state == 1 and neighbor_cell_sum in (2,3):
+            elif c == 1 and neighbor_cell_sum in (2,3):
                 next_space[i,j] = 1
             else:
                 next_space[i,j] = 0
