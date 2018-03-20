dec8e02f39ea2bd1434e751571a0552efe8e36aa
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-03-21 00:05:56 +0900

    small fix

--- chap05_embodiment/chaos_vehicle.py
+++ chap05_embodiment/chaos_vehicle.py
@@ -7,9 +7,9 @@ from alifebook_lib.simulator import TwoWheelVehicleRobotSimulator
 t3 = T3(omega0 = 0.9, omega1 = 0.3, epsilon = 0.1)
 
 def controll_func(sensor_data):
-    x, y = t3.next()
     t3.set_parameters(omega0 = 0.5 + 0.5*sensor_data['left_distance'])
     t3.set_parameters(omega1 = 0.5 + 0.5*sensor_data['right_distance'])
+    x, y = t3.next()
     left_wheel_speed = 50 * x
     right_wheel_speed = 50 * y
     return left_wheel_speed, right_wheel_speed
