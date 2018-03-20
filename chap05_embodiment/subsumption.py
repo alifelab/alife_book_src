6ea3ab4aa9e90c24dbbced46afeafc565345deff
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-03-21 00:49:11 +0900

    implemented chaos wonder/explore module on subsumption (chap05)

--- chap05_embodiment/subsumption.py
+++ chap05_embodiment/subsumption.py
@@ -48,10 +48,8 @@ class AvoidModule(SubsumptionModule):
         pass
 
     def on_update(self):
-        #self.set_output("left_wheel_speed",  30 * (1 - self.get_input("right_distance")))
-        #self.set_output("right_wheel_speed", 30 * (1 - self.get_input("left_distance")))
-        self.set_output("left_wheel_speed",  20 + 20 * self.get_input("left_distance"))
-        self.set_output("right_wheel_speed", 20 + 20 * self.get_input("right_distance"))
+        self.set_output("left_wheel_speed",  10 + 30 * self.get_input("left_distance"))
+        self.set_output("right_wheel_speed", 10 + 30 * self.get_input("right_distance"))
 
 
 class WanderModule(SubsumptionModule):
@@ -62,11 +60,10 @@ class WanderModule(SubsumptionModule):
         self.add_child_module('avoid', AvoidModule())
 
     def on_update(self):
-        # count steps not senser activated
-        if self.get_input("right_distance") > 0 and self.get_input("left_distance") > 0:
-            self.counter = 0
-        else:
+        if self.get_input("right_distance") < 0.1 and self.get_input("left_distance") < 0.1:
             self.counter = (self.counter + 1) % self.TURN_END_STEP
+        else:
+            self.counter = 0
         if self.counter < self.TURN_START_STEP:
             # not inhibit child(avoid) module
             # through child module's output to output
@@ -86,19 +83,51 @@ class WanderModule(SubsumptionModule):
 
 class ExploreModule(SubsumptionModule):
     def on_init(self):
-        self.counter = 0
         self.add_child_module('wander', WanderModule())
 
     def on_update(self):
         if self.get_input('feed_touching'):
+            print("feeding")
             # speed down to explore
-            self.set_output("left_wheel_speed",  3)
-            self.set_output("right_wheel_speed", 3)
+            self.set_output("left_wheel_speed",  0)
+            self.set_output("right_wheel_speed", 0)
         else:
             # not inhibit child(wander) module
             # through child module's output to output
             self.set_output("left_wheel_speed",  self.child_modules['wander'].get_output("left_wheel_speed"))
             self.set_output("right_wheel_speed", self.child_modules['wander'].get_output("right_wheel_speed"))
+
+
+from t3 import T3
+
+class ChaosWanderModule(SubsumptionModule):
+    def on_init(self):
+        self.add_child_module('avoid', AvoidModule())
+        self.t3 = T3(omega0 = 0.9, omega1 = 0.3, epsilon = 0.1)
+        self.t3.set_parameters(omega0 = np.random.rand())
+        self.t3.set_parameters(omega1 = np.random.rand())
+
+    def on_update(self):
+        x, y = self.t3.next()  # update chaos dynamics
+        if self.get_input("right_distance") < 0.001 and self.get_input("left_distance") < 0.001:
+            # without thouching distance sensor (on free space), vehicle behave chaotic
+            left_wheel_speed = 50 * x
+            right_wheel_speed = 50 * y
+            self.set_output("left_wheel_speed",  left_wheel_speed)
+            self.set_output("right_wheel_speed", right_wheel_speed)
+        else:
+            # when distance sensor touch obstacle, child module (avoid module) will be activated
+            self.set_output("left_wheel_speed",  self.child_modules['avoid'].get_output("left_wheel_speed"))
+            self.set_output("right_wheel_speed", self.child_modules['avoid'].get_output("right_wheel_speed"))
+            # and update chaos module's parameters
+            self.t3.set_parameters(omega0 = np.random.rand())
+            self.t3.set_parameters(omega1 = np.random.rand())
+
+
+class ChaosExploreModule(ExploreModule):
+    def on_init(self):
+        self.add_child_module('wander', ChaosWanderModule())
+
 
 
 ######################
@@ -107,6 +136,9 @@ class ExploreModule(SubsumptionModule):
 #controller = AvoidModule()  # same as braitenberg vehicle (layer0)
 #controller = WanderModule()  # add wandering module (layer1)
 #controller = ExploreModule() # add explore module (layer2)
+#controller = ChaosWanderModule()  # chaos version of wandering module
+controller = ChaosExploreModule()  # chaos wandering + explore module
+
 
 def controll_func(sensor_data):
     controller.set_inputs(sensor_data)
@@ -114,6 +146,6 @@ def controll_func(sensor_data):
     return controller.get_output('left_wheel_speed'), controller.get_output('right_wheel_speed')
 
 if __name__ == '__main__':
-    sim = TwoWheelVehicleRobotSimulator(controll_func, obstacle_num=3, feed_num=40)
+    sim = TwoWheelVehicleRobotSimulator(controll_func, obstacle_num=5, feed_num=40)
     sim.controll_func = controll_func
     sim.run()
