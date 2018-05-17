a946028163dce2671c17b3a1cb70671f93cd8800
Lana <lana.sinapayen@gmail.com>
2018-05-17 23:25:33 +0900

    correct spelling of  function

--- chap05_embodiment/braitenberg_vehicle.py
+++ chap05_embodiment/braitenberg_vehicle.py
@@ -7,11 +7,11 @@ import numpy as np
 from alifebook_lib.simulators import TwoWheelVehicleRobotSimulator
 
 
-def controll_func(sensor_data):
+def control_func(sensor_data):
     left_wheel_speed = 20 + 20 * sensor_data["left_distance"]
     right_wheel_speed = 20 + 20 * sensor_data["right_distance"]
     return left_wheel_speed, right_wheel_speed
 
-sim = TwoWheelVehicleRobotSimulator(controll_func, obstacle_num=5)
-sim.controll_func = controll_func
+sim = TwoWheelVehicleRobotSimulator(control_func, obstacle_num=5)
+sim.control_func = control_func
 sim.run()
