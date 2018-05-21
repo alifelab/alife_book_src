a894edc3f7de8493e035da65edc627cae725fb45
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-05-21 15:55:09 +0900

    no message

--- test/vispy_test10.py
+++ test/vispy_test10.py
@@ -1,150 +1,157 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vispy: gallery 20
-
+# -----------------------------------------------------------------------------
+# Copyright (c) Vispy Development Team. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
 """
-Example demonstrating simulation of fireworks using point sprites.
-(adapted from the "OpenGL ES 2.0 Programming Guide")
-This example demonstrates a series of explosions that last one second. The
-visualization during the explosion is highly optimized using a Vertex Buffer
-Object (VBO). After each explosion, vertex data for the next explosion are
-calculated, such that each explostion is unique.
+Tutorial: Creating Visuals
+--------------------------
+This tutorial is intended to guide developers who are interested in creating
+new subclasses of Visual. In most cases, this will not be necessary because
+vispy's base library of visuals will be sufficient to create complex scenes as
+needed. However, there are cases where a particular visual effect is desired
+that is not supported in the base library, or when a custom visual is needed to
+optimize performance for a specific use case.
+The purpose of a Visual is to encapsulate a single drawable object. This
+drawable can be as simple or complex as desired. Some of the simplest visuals
+draw points, lines, or triangles, whereas more complex visuals invove multiple
+drawing stages or make use of sub-visuals to construct larger objects.
+In this example we will create a very simple Visual that draws a rectangle.
+Visuals are defined by:
+1. Creating a subclass of vispy.visuals.Visual that specifies the GLSL code
+   and buffer objects to use.
+2. Defining a _prepare_transforms() method that will be called whenever the
+   user (or scenegraph) assigns a new set of transforms to the visual.
 """
-
-import time
+from vispy import app, gloo, visuals, scene
 import numpy as np
-from vispy import gloo, app
 
-# import vispy
-# vispy.use('pyside', 'es2')
-
-
-# Create a texture
-radius = 32
-im1 = np.random.normal(
-    0.8, 0.3, (radius * 2 + 1, radius * 2 + 1)).astype(np.float32)
-
-# Mask it with a disk
-L = np.linspace(-radius, radius, 2 * radius + 1)
-(X, Y) = np.meshgrid(L, L)
-im1 *= np.array((X ** 2 + Y ** 2) <= radius * radius, dtype='float32')
-
-# Set number of particles, you should be able to scale this to 100000
-N = 10000
-
-# Create vertex data container
-data = np.zeros(N, [('a_lifetime', np.float32, 1),
-                    ('a_startPosition', np.float32, 3),
-                    ('a_endPosition', np.float32, 3)])
-
-
-VERT_SHADER = """
-uniform float u_time;
-uniform vec3 u_centerPosition;
-attribute float a_lifetime;
-attribute vec3 a_startPosition;
-attribute vec3 a_endPosition;
-varying float v_lifetime;
-void main () {
-    if (u_time <= a_lifetime)
-    {
-        gl_Position.xyz = a_startPosition + (u_time * a_endPosition);
-        gl_Position.xyz += u_centerPosition;
-        gl_Position.y -= 1.0 * u_time * u_time;
-        gl_Position.w = 1.0;
-    }
-    else
-        gl_Position = vec4(-1000, -1000, 0, 0);
-    v_lifetime = 1.0 - (u_time / a_lifetime);
-    v_lifetime = clamp(v_lifetime, 0.0, 1.0);
-    gl_PointSize = (v_lifetime * v_lifetime) * 40.0;
+# Define a simple vertex shader. We use $template variables as placeholders for
+# code that will be inserted later on. In this example, $position will become
+# an attribute, and $transform will become a function. Important: using
+# $transform in this way ensures that users of this visual will be able to
+# apply arbitrary transformations to it.
+vertex_shader = """
+void main() {
+   gl_Position = $transform(vec4($position, 0, 1));
 }
 """
 
-# Deliberately add precision qualifiers to test automatic GLSL code conversion
-FRAG_SHADER = """
-precision highp float;
-uniform sampler2D texture1;
-uniform vec4 u_color;
-varying float v_lifetime;
-uniform highp sampler2D s_texture;
-void main()
-{
-    highp vec4 texColor;
-    texColor = texture2D(s_texture, gl_PointCoord);
-    gl_FragColor = vec4(u_color) * texColor;
-    gl_FragColor.a *= v_lifetime;
+# Very simple fragment shader. Again we use a template variable "$color", which
+# allows us to decide later how the color should be defined (in this case, we
+# will just use a uniform red color).
+fragment_shader = """
+void main() {
+  gl_FragColor = $color;
 }
 """
 
 
-class Canvas(app.Canvas):
+# Start the new Visual class.
+# By convention, all Visual subclass names end in 'Visual'.
+# (Custom visuals may ignore this convention, but for visuals that are built
+# in to vispy, this is required to ensure that the VisualNode subclasses are
+# generated correctly.)
+class MyRectVisual(visuals.Visual):
+    """Visual that draws a red rectangle.
 
-    def __init__(self):
-        app.Canvas.__init__(self, size=(800, 600))
+    Parameters
+    ----------
+    x : float
+        x coordinate of rectangle origin
+    y : float
+        y coordinate of rectangle origin
+    w : float
+        width of rectangle
+    h : float
+        height of rectangle
 
-        # Create program
-        self._program = gloo.Program(VERT_SHADER, FRAG_SHADER)
-        self._program.bind(gloo.VertexBuffer(data))
-        self._program['s_texture'] = gloo.Texture2D(im1)
+    All parameters are specified in the local (arbitrary) coordinate system of
+    the visual. How this coordinate system translates to the canvas will
+    depend on the transformation functions used during drawing.
+    """
 
-        # Create first explosion
-        self._new_explosion()
+    # There are no constraints on the signature of the __init__ method; use
+    # whatever makes the most sense for your visual.
+    def __init__(self, x, y, w, h):
+        # Initialize the visual with a vertex shader and fragment shader
+        visuals.Visual.__init__(self, vertex_shader, fragment_shader)
 
-        # Enable blending
-        gloo.set_state(blend=True, clear_color='black',
-                       blend_func=('src_alpha', 'one'))
+        # vertices for two triangles forming a rectangle
+        self.vbo = gloo.VertexBuffer(np.array([
+            [x, y], [x+w, y], [x+w, y+h],
+            [x, y], [x+w, y+h], [x, y+h]
+        ], dtype=np.float32))
 
-        gloo.set_viewport(0, 0, self.physical_size[0], self.physical_size[1])
+        # Assign values to the $position and $color template variables in
+        # the shaders. ModularProgram automatically handles generating the
+        # necessary attribute and uniform declarations with unique variable
+        # names.
+        self.shared_program.vert['position'] = self.vbo
+        self.shared_program.frag['color'] = (1, 0, 0, 1)
+        self._draw_mode = 'triangles'
 
-        self._timer = app.Timer('auto', connect=self.update, start=True)
+    def _prepare_transforms(self, view):
+        # This method is called when the user or the scenegraph has assigned
+        # new transforms to this visual (ignore the *view* argument for now;
+        # we'll get to that later). This method is thus responsible for
+        # connecting the proper transform functions to the shader program.
 
-        self.show()
-
-    def on_resize(self, event):
-        width, height = event.physical_size
-        gloo.set_viewport(0, 0, width, height)
-
-    def on_close(self, event):
-        print("close event")
-
-    def on_draw(self, event):
-
-        # Clear
-        gloo.clear()
-
-        # Draw
-        self._program['u_time'] = time.time() - self._starttime
-        self._program.draw('points')
-
-        # New explosion?
-        if time.time() - self._starttime > 1.5:
-            self._new_explosion()
+        # The most common approach here is to simply take the complete
+        # transformation from visual coordinates to render coordinates. Later
+        # tutorials detail more complex transform handling.
+        view.view_program.vert['transform'] = view.get_transform()
 
 
+# At this point the visual is ready to use, but it takes some extra effort to
+# set up a Canvas and TransformSystem for drawing (the examples in
+# examples/basics/visuals/ all follow this approach).
+#
+# An easier approach is to make the visual usable in a scenegraph, in which
+# case the canvas will take care of drawing the visual and setting up the
+# TransformSystem for us.
+#
+# To be able to use our new Visual in a scenegraph, it needs to be
+# a subclass of scene.Node. In vispy we achieve this by creating a parallel
+# set of classes that inherit from both Node and each Visual subclass.
+# This can be done automatically using scene.visuals.create_visual_node():
+MyRect = scene.visuals.create_visual_node(MyRectVisual)
 
-    def _new_explosion(self):
-        # New centerpos
-        centerpos = np.random.uniform(-0.5, 0.5, (3,))
-        self._program['u_centerPosition'] = centerpos
+# By convention, these classes have the same name as the Visual they inherit
+# from, but without the 'Visual' suffix.
 
-        # New color, scale alpha with N
-        alpha = 1.0 / N ** 0.08
-        color = np.random.uniform(0.1, 0.9, (3,))
+# The auto-generated class MyRect is basically equivalent to::
+#
+#     class MyRect(MyRectVisual, scene.Node):
+#        def __init__(self, *args, **kwds):
+#            parent = kwds.pop('parent', None)
+#            name = kwds.pop('name', None)
+#            MyRectVisual.__init__(self, *args, **kwds)
+#            Node.__init__(self, parent=parent, name=name)
+#
 
-        self._program['u_color'] = tuple(color) + (alpha,)
+from vispy.scene.visuals import *
 
-        # Create new vertex data
-        data['a_lifetime'] = np.random.normal(2.0, 0.5, (N,))
-        data['a_startPosition'] = np.random.normal(0.0, 0.2, (N, 3))
-        data['a_endPosition'] = np.random.normal(0.0, 1.2, (N, 3))
+# Finally we will test the visual by displaying in a scene.
 
-        # Set time to zero
-        self._starttime = time.time()
+# Create a canvas to display our visual
+canvas = scene.SceneCanvas(keys='interactive', show=True)
 
-import vispy
+# Create two instances of MyRect, each using canvas.scene as their parent
+rects = [MyRect(100, 100, 200, 300, parent=canvas.scene),
+         MyRect(500, 100, 200, 300, parent=canvas.scene)]
 
+#x = scene.visuals.create_visual_node(Image)
+Image(np.random.rand(100,100), parent=canvas.scene)
+Ellipse((100,100), color='red', radius=1000)
+
+# To test that the user-specified transforms work correctly, I'll rotate
+# one rectangle slightly.
+tr = visuals.transforms.MatrixTransform()
+tr.rotate(5, (0, 0, 1))
+rects[1].transform = tr
+
+# ..and optionally start the event loop
 if __name__ == '__main__':
-    #vispy.use('Glfw')
-    c = Canvas()
     app.run()
+    print("test")
