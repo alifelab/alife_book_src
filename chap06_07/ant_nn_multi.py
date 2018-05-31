2258b2c773d1fffa69292512961331eb10500334
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-05-31 19:39:35 +0900

    no message

--- chap06_07/ant_nn_multi.py
+++ chap06_07/ant_nn_multi.py
@@ -4,77 +4,48 @@
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, InputLayer
 from alifebook_lib.simulators import AntSimulator
-from alifebook_lib.utils.nn_ga_utils import *
+from ant_nn_utils import generate_nn_model, generate_action, decode_weights, CONTEXT_NEURON_NUM
 
-
-CONTEXT_NN_NUM = 2
-
-# action function for agent NN
-def action(nn_model, observation, prev_context_val):
-    nn_input = np.r_[observation, prev_context_val]
-    nn_input = nn_input.reshape(1, len(nn_input))
-    nn_output = nn_model.predict(nn_input)
-    act = np.array([nn_output[0][:2]])
-    context_val = nn_output[0][2:]
-    return act, context_val
-
-
-# function to generate NN model and initial context values from gene
-def gen_nn_model(gene):
-    nn_model = Sequential()
-    nn_model.add(InputLayer((7+CONTEXT_NN_NUM,)))
-    nn_model.add(Dense(4, activation='sigmoid'))
-    nn_model.add(Dense(2+CONTEXT_NN_NUM, activation='sigmoid'))
-    decode_weights(nn_model, gene)
-    context_val = np.zeros(CONTEXT_NN_NUM)
-    return nn_model, context_val
-
-
-# generage gradation color by value(0-1)
-# ex) 0.0 -> red, 0.5 -> green, 1.0 -> blue
-def gen_gradation_color(x):
-    r = max(-2.0 * x + 1.0, 0.0)
-    g = min(2.0 * x, -2.0 * x + 2.0)
-    b = max(2.0 * x - 1.0, 0.0)
-    return (r, g, b)
-
-
-# setup agents NN models and initial context values by commandline argments
 agent_num = []
 agent_nn_model_list = []
 agent_nn_context_val_list = []
 
 for i in range(1, len(sys.argv), 2):
-    g = np.load(sys.argv[i])
-    n = int(sys.argv[i+1])
-    agent_num.append(n)
-    for j in range(n):
-        m, c = gen_nn_model(g)
-        agent_nn_model_list.append(m)
-        agent_nn_context_val_list.append(c)
-
+    gene = np.load(sys.argv[i])
+    num = int(sys.argv[i+1])
+    agent_num.append(num)
+    for j in range(num):
+        nn_model = generate_nn_model()
+        decode_weights(nn_model, gene)
+        context_val = np.zeros(CONTEXT_NEURON_NUM)
+        agent_nn_model_list.append(nn_model)
+        agent_nn_context_val_list.append(context_val)
 
 N = np.sum(agent_num)
-sim = AntSimulator(N, decay_rate=0.999, secretion=True)
+action = np.empty((N, 2)) # 各エージェントのアクションを収めるための (Nx2) の配列
 
-# set agent color depends on gene
+simulator = AntSimulator(N, decay_rate=0.999, secretion=True)
+
+# エージェントの遺伝子ファイル毎に色をセットする
 idx = 0
 if len(agent_num) > 1:
     for i, n in enumerate(agent_num):
+        # xには0-1の間の等間隔の値が入る
         x = i / (len(agent_num) - 1)
-        c = gen_gradation_color(x)
+        # xに応じてグラデーション色を生成
+        r = max(-2.0 * x + 1.0, 0.0)
+        g = min(2.0 * x, -2.0 * x + 2.0)
+        b = max(2.0 * x - 1.0, 0.0)
+        color = (r, g, b)
         for j in range(n):
-            sim.set_agent_color(idx, c)
+            simulator.set_agent_color(idx, color)
             idx += 1
 
-act = np.empty((N, 2))  # empty matrix (Nx2) to contain action vectors
-obs = sim.reset()
-while True:
+while simulator:
+    sensor_data = simulator.get_sensor_data()
     for i in range(N):
-        a, c = action(agent_nn_model_list[i], obs[i], agent_nn_context_val_list[i])
-        act[i] = a
+        a, c = generate_action(agent_nn_model_list[i], sensor_data[i], agent_nn_context_val_list[i])
+        action[i] = a
         agent_nn_context_val_list[i] = c
-    obs = sim.step(act)
+    simulator.update(action)
