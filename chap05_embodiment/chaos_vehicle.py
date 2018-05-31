aaaa5ecfec8d66cdbe0853bf2d431a669f033b66
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-05-31 23:19:13 +0900

    fixed bug of chaos_vehcle

--- chap05_embodiment/chaos_vehicle.py
+++ chap05_embodiment/chaos_vehicle.py
@@ -7,13 +7,14 @@ import numpy as np
 from t3 import T3
 from alifebook_lib.simulators import VehicleSimulator
 
+simulator = VehicleSimulator(obstacle_num=0)
 t3 = T3(omega0 = 0.9, omega1 = 0.3, epsilon = 0.1)
 
-simulator = VehicleSimulator(obstacle_num=0)
+t3.set_parameters(omega0 = np.random.rand())
+t3.set_parameters(omega1 = np.random.rand())
+
 while simulator:
     sensor_data = simulator.get_sensor_data()
-    t3.set_parameters(omega0 = 0.5 + 0.5*sensor_data['left_distance'])
-    t3.set_parameters(omega1 = 0.5 + 0.5*sensor_data['right_distance'])
     x, y = t3.next()
     left_wheel_speed = 50 * x
     right_wheel_speed = 50 * y
