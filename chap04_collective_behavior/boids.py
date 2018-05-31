681245117035c29a31ae31d717e682d8e7220323
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-05-31 10:37:50 +0900

    refactering boids.

--- chap04_collective_behavior/boids.py
+++ chap04_collective_behavior/boids.py
@@ -14,11 +14,15 @@ N = 256
 # 力の強さ
 COHESION_FORCE = 0.008
 SEPARATION_FORCE = 0.04
+#SEPARATION_FORCE = 0.4
 ALIGNMENT_FORCE = 0.06
 # 力の働く距離
 COHESION_DISTANCE = 0.05
 SEPARATION_DISTANCE = 0.01
 ALIGNMENT_DISTANCE = 0.05
+#COHESION_DISTANCE = 0.5
+#SEPARATION_DISTANCE = 0.05
+#ALIGNMENT_DISTANCE = 0.1
 # 力の働く角度
 COHESION_ANGLE = np.pi / 2
 SEPARATION_ANGLE = np.pi / 2
@@ -26,20 +30,23 @@ ALIGNMENT_ANGLE = np.pi / 3
 # 速度の上限/下限
 MIN_VEL = 0.001
 MAX_VEL = 0.005
-# 中心力
-# CENTRAL_FORCE = 0.0001
-# CENTER_POSITION = np.zeros(3)
+#MIN_VEL = 0.005
+#MAX_VEL = 0.03
+# 境界で働く力（0にすると自由境界）
+BOUNDARY_FORCE = 0.001
 
 # 位置と速度
 x = np.random.rand(N, 3) * 2 - 1
-v = np.random.rand(N, 3) * MIN_VEL
+v = (np.random.rand(N, 3) * 2 - 1 ) * MIN_VEL
+
+# cohesion, separation, alignmentの３つの力を代入する変数
+dv_coh = np.empty((N,3))
+dv_sep = np.empty((N,3))
+dv_ali = np.empty((N,3))
+# 境界で働く力を代入する変数
+dv_boundary = np.empty((N,3))
 
 while visualizer:
-    # cohesion, separation, alignmentの３つの力を代入する変数
-    dv_coh = np.zeros((N,3))
-    dv_sep = np.zeros((N,3))
-    dv_ali = np.zeros((N,3))
-
     for i in range(N):
         # ここで計算する個体の位置と速度
         x_this = x[i]
@@ -55,15 +62,13 @@ while visualizer:
         sep_agents_x = x_that[ (distance < SEPARATION_DISTANCE) & (angle < SEPARATION_ANGLE) ]
         ali_agents_v = v_that[ (distance < ALIGNMENT_DISTANCE) & (angle < ALIGNMENT_ANGLE) ]
         # 各力の計算
-        if (len(coh_agents_x) > 0):
-            dv_coh[i] = COHESION_FORCE * (np.average(coh_agents_x, axis=0) - x_this)
-        if (len(sep_agents_x) > 0):
-            dv_sep[i] = SEPARATION_FORCE * np.sum(x_this - sep_agents_x, axis=0)
-        if (len(ali_agents_v) > 0):
-            dv_ali[i] = ALIGNMENT_FORCE * (np.average(ali_agents_v, axis=0) - v_this)
+        dv_coh[i] = COHESION_FORCE * (np.average(coh_agents_x, axis=0) - x_this) if (len(coh_agents_x) > 0) else 0
+        dv_sep[i] = SEPARATION_FORCE * np.sum(x_this - sep_agents_x, axis=0) if (len(sep_agents_x) > 0) else 0
+        dv_ali[i] = ALIGNMENT_FORCE * (np.average(ali_agents_v, axis=0) - v_this) if (len(ali_agents_v) > 0) else 0
+        dist_center = np.linalg.norm(x_this) # 原点からの距離
+        dv_boundary[i] = - BOUNDARY_FORCE * x_this * (dist_center - 1) / dist_center if (dist_center > 1) else 0
     # 速度のアップデートと上限/下限のチェック
-    v += dv_coh + dv_sep + dv_ali
-    # v += CENTRAL_FORCE * (CENTER_POSITION - x)
+    v += dv_coh + dv_sep + dv_ali + dv_boundary
     for i in range(N):
         v_abs = np.linalg.norm(v[i])
         if (v_abs < MIN_VEL):
--- chap04_collective_behavior/boids_prey.py
+++ chap04_collective_behavior/boids_prey.py
@@ -10,68 +10,76 @@ from alifebook_lib.visualizers import SwarmVisualizer
 visualizer = SwarmVisualizer()
 
 # シミュレーションパラメタ
-N = 64
-PREY_FORCE = 0.0001
-COHESION_FORCE = 0.01
-SEPARATION_FORCE = 0.01
-ALIGNMENT_FORCE = 0.1
-# all interaction distance and angle take same value for simplify.
-INTERACTION_DISTANCE = 0.05
-INTERACTION_ANGLE = np.pi / 3
-MIN_VEL = 0.001
-MAX_VEL = 0.005
-PREY_MOVEMENT_STEP = 50
+N = 256
+# 力の強さ
+COHESION_FORCE = 0.008
+SEPARATION_FORCE = 0.4
+ALIGNMENT_FORCE = 0.06
+# 力の働く距離
+COHESION_DISTANCE = 0.5
+SEPARATION_DISTANCE = 0.05
+ALIGNMENT_DISTANCE = 0.1
+# 力の働く角度
+COHESION_ANGLE = np.pi / 2
+SEPARATION_ANGLE = np.pi / 2
+ALIGNMENT_ANGLE = np.pi / 3
+# 速度の上限/下限
+MIN_VEL = 0.005
+MAX_VEL = 0.03
+# 境界で働く力（0にすると自由境界）
+BOUNDARY_FORCE = 0.001
+# 餌に吸引される力と動かす間隔
+PREY_FORCE = 0.0005
+PREY_MOVEMENT_STEP = 150
 
 # 位置と速度
-x = np.random.rand(N, 3) * 0.1
-v = np.random.rand(N, 3) * MIN_VEL
-# preyの位置
-prey_x = np.random.rand(1, 3) * 0.5
+x = np.random.rand(N, 3) * 2 - 1
+v = (np.random.rand(N, 3) * 2 - 1 ) * MIN_VEL
+# 餌の位置
+prey_x = np.random.rand(1, 3) * 2 - 1
+
+# cohesion, separation, alignmentの３つの力を代入する変数
+dv_coh = np.empty((N,3))
+dv_sep = np.empty((N,3))
+dv_ali = np.empty((N,3))
+# 境界で働く力を代入する変数
+dv_boundary = np.empty((N,3))
 
 t = 0
 while visualizer:
-    # 3 force, cohesion, separation and alignment
-    dv_coh = np.zeros((N,3))
-    dv_sep = np.zeros((N,3))
-    dv_ali = np.zeros((N,3))
-
     for i in range(N):
-        # xi and vi are position and velocity of target agent
-        xi = x[i]
-        vi = v[i]
-        # xj and vj are list of position and velocity of other boids
-        xj = np.delete(x, i, axis=0)
-        vj = np.delete(v, i, axis=0)
-        # list of distance and angle
-        dist = np.linalg.norm(xj - xi, axis=1)
-        angle = np.arccos(np.dot(vi, (xj-xi).T) / (np.linalg.norm(vi) * np.linalg.norm((xj-xi), axis=1)))
-        # extract agents in interaction area.
-        interact_agents_x = xj[ (dist < INTERACTION_DISTANCE) & (angle < INTERACTION_ANGLE) ]
-        interact_agents_v = vj[ (dist < INTERACTION_DISTANCE) & (angle < INTERACTION_ANGLE) ]
-        # calculate several forces.
-        if (len(interact_agents_x) > 0):
-            dv_coh[i] = COHESION_FORCE * (np.average(interact_agents_x, axis=0) - xi)
-        if (len(interact_agents_x) > 0):
-            dv_sep[i] = SEPARATION_FORCE * np.sum(xi - interact_agents_x, axis=0)
-        if (len(interact_agents_v) > 0):
-            dv_ali[i] = ALIGNMENT_FORCE * (np.average(interact_agents_v, axis=0) - vi)
-    v += dv_coh + dv_sep + dv_ali
-
-    # PREY MODEL
+        # ここで計算する個体の位置と速度
+        x_this = x[i]
+        v_this = v[i]
+        # それ以外の個体の位置と速度の配列
+        x_that = np.delete(x, i, axis=0)
+        v_that = np.delete(v, i, axis=0)
+        # 個体間の距離と角度
+        distance = np.linalg.norm(x_that - x_this, axis=1)
+        angle = np.arccos(np.dot(v_this, (x_that-x_this).T) / (np.linalg.norm(v_this) * np.linalg.norm((x_that-x_this), axis=1)))
+        # 各力が働く範囲内の個体のリスト
+        coh_agents_x = x_that[ (distance < COHESION_DISTANCE) & (angle < COHESION_ANGLE) ]
+        sep_agents_x = x_that[ (distance < SEPARATION_DISTANCE) & (angle < SEPARATION_ANGLE) ]
+        ali_agents_v = v_that[ (distance < ALIGNMENT_DISTANCE) & (angle < ALIGNMENT_ANGLE) ]
+        # 各力の計算
+        dv_coh[i] = COHESION_FORCE * (np.average(coh_agents_x, axis=0) - x_this) if (len(coh_agents_x) > 0) else 0
+        dv_sep[i] = SEPARATION_FORCE * np.sum(x_this - sep_agents_x, axis=0) if (len(sep_agents_x) > 0) else 0
+        dv_ali[i] = ALIGNMENT_FORCE * (np.average(ali_agents_v, axis=0) - v_this) if (len(ali_agents_v) > 0) else 0
+        dist_center = np.linalg.norm(x_this) # 原点からの距離
+        dv_boundary[i] = - BOUNDARY_FORCE * x_this * (dist_center - 1) / dist_center if (dist_center > 1) else 0
+    # 速度のアップデートと上限/下限のチェック
+    v += dv_coh + dv_sep + dv_ali + dv_boundary
     v += PREY_FORCE * (prey_x - x) / np.linalg.norm((prey_x - x), axis=1, keepdims=True)**2
     if t % PREY_MOVEMENT_STEP == 0:
-        prey_x = np.random.rand(1, 3) * 0.5
+        prey_x = np.random.rand(1, 3) * 2 - 1
+        visualizer.set_markers(prey_x)
     t += 1
-
-    # check min/max velocity.
     for i in range(N):
         v_abs = np.linalg.norm(v[i])
         if (v_abs < MIN_VEL):
             v[i] = MIN_VEL * v[i] / v_abs
         elif (v_abs > MAX_VEL):
             v[i] = MAX_VEL * v[i] / v_abs
-
-    # update
+    # 位置のアップデート
     x += v
-
-    visualizer.update(x, v, markers_x=prey_x, range=(0, 0.5))
+    visualizer.update(x, v)
