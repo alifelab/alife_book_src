810de8833e3cf81997437b172eff60aae62eaff0
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-06-01 00:14:27 +0900

    removed unused files.

--- etc/matplotlib_animation/circle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import matplotlib.pyplot as plt
-
-ax = plt.axes(xlim=(0, 10), ylim=(0, 10), aspect='equal')
-circle = plt.Circle((5, 5), 0.75)
-ax.add_patch(circle)
-
-plt.show()
--- test/pybullet_gym_test/gym_test04.py
+++
@@ -1,6 +0,0 @@
-
-
-class HopperBulletEnv(WalkerBaseBulletEnv):
-	def __init__(self):
-		self.robot = Hopper()
-		WalkerBaseBulletEnv.__init__(self, self.robot)
